4b9f584d60c574d1eb62b4249b748c064f0c1364
Subrahmanyam Gampala <sgampal1@binghamton.edu>
2021-04-21 19:11:56 +0000

    Successfully getting omniauth hash values

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,30 @@
 class SessionsController < ApplicationController
-  def clear
+  def nested_hash nh, indent
+    puts indent + "VALUE is a nested hash"
+    indent += '  '
+    nh.each_pair do |key, value|
+      puts indent + "KEY: #{key}"
+      if value.kind_of?(Hash)
+        nested_hash(value, indent)
+      else
+        puts indent + "VALUE:  #{value}"
+      end     
+    end
   end
-
-  def debug
-  end
-
-  def new
-  end
-
+    
   def create
-  end
-
-  def failure
-  end
-
-  def destroy
+    auth_hash = request.env['omniauth.auth']
+      
+    puts '\n raw auth_hash\n'
+    p auth_hash
+    puts '\n\nauth_hash by key\n'
+    auth_hash.each_pair do |key, value|
+      puts "\nKEY: #{key}"
+      if value.kind_of?(Hash)
+        nested_hash(value, '')
+      else
+        puts "Value: #{value}"
+      end
+    end
   end
 end
